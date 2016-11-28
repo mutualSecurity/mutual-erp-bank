5cb2a0373555a3f17f8a7969e4e25961abb69860
parkash067 <pk_bscs@yahoo.com>
2016-11-28 15:53:43 +0500

    change name of date to from and + date_to +amount to actual_Amount

--- mutual_expenses/mutualexpenses.py
+++ mutual_expenses/mutualexpenses.py
@@ -4,7 +4,8 @@ class mutual_expenses(osv.osv):
     _inherit = "hr.expense.expense",
     _columns = {
         'partner_id_expense': fields.many2one('res.partner', 'Customer', required=True),
-        'test': fields.char('test')
+        'technician_amount': fields.float('Technician Amount'),
+        'date_to':fields.date('To', store=True),
     }
 
 mutual_expenses()
