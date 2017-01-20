daef451f906074f419c99825ec2d12b7efb1cbdf
parkash067 <pk_bscs@yahoo.com>
2017-01-20 11:56:24 +0500

    changes in mutual_issues(implementation of messaging logs + remove restriction from bank remarks) mutual_invoice(implementation of filters + cheque no./date/po/dd) mutual_contracts(some filters + show cs and branch code in tree vxiew)

--- mutual_contracts/mutual_contracts.py
+++ mutual_contracts/mutual_contracts.py
@@ -5,5 +5,9 @@ class invoice_csnumber(osv.osv):
     _inherit = 'account.analytic.account'
     _columns = {
         'cs_number': fields.related('partner_id', 'cs_number', type='char', size=12, string='CS Number', readonly=True),
+        'branch_code': fields.related('partner_id','branch_code',type='char',string='Branch Code',readonly=True),
+        'bank_code': fields.related('partner_id','bank_code',type='char',string='Bank Code',readonly=True),
+        'address': fields.related('partner_id','street',type='char',string='Address',readonly=True),
+        'city': fields.related('partner_id','city',type='char',string='City',readonly=True),
     }
 
--- mutual_invoice/mutual_invoice.py
+++ mutual_invoice/mutual_invoice.py
@@ -19,7 +19,10 @@ class invoice_csnumber(osv.osv):
         'bank_cs_invoice': fields.related('partner_id', 'cs_number', type='char', size=12, string='CS Number',readonly=True),
         'bank_code_invoice': fields.related('partner_id','bank_code',type='char', size=12,string='Bank code',readonly=True),
         'branch_code_invoice': fields.related('partner_id', 'branch_code', type='char', size=12, string='Branch code',readonly=True),
-        'remarks': fields.text('Follow Up', store=True)
+        'remarks': fields.text('Follow Up', store=True),
+        'po_dd':fields.selection([('PO','PO'),('DD','DD')], 'PO/DD', store=True),
+        'cheque_date': fields.date('Cheque Received Date', store=True, track_visibility='onchange'),
+        'cheque_no':fields.char('Cheque no.',store=True,track_visibility='onchange')
     }
 
     @api.multi
