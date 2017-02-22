abaacff3beb432b0fa9f09275b134784d73a13f2
parkash067 <pk_bscs@yahoo.com>
2017-02-22 21:47:56 +0500

    add guard tracking module

---
+++ mutual_guard_tracking/guard_tracking.py
@@ -0,0 +1,22 @@
+from openerp.osv import fields, osv
+
+
+class mutual_guard_tracking(osv.osv):
+    _name = "mutual.guard.tracking"
+    _columns = {
+        'bank_code': fields.char('Bank Code', store=True, readonly=True),
+        'branch_code': fields.char('Branch Code', store=True, readonly=True),
+        'address': fields.char('Address', store=True, readonly=True),
+        'visit_time': fields.char('Visit Time', store=True, readonly=True),
+        'visit_date': fields.date('Visit Date', store=True, readonly=True),
+        'card_no': fields.char('RF_ID', store=True),
+        'device_no': fields.char('Device#', store=True),
+    }
+
+
+class mutual_device_tracking(osv.osv):
+    _name = "mutual.device.tracking"
+    _columns = {
+        'scanner_id': fields.char('Scanner ID', store=True),
+        'force_id': fields.char('Force ID', store=True),
+    }
