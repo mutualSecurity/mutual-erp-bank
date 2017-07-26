8762eaabcc56fe8446bd8dd5f6364606712db75f
parkash067 <pk_bscs@yahoo.com>
2017-07-26 17:08:31 +0500

    Issue fixed

--- mutual_guard_tracking/wizard/wizard_missingvisits.py
+++ mutual_guard_tracking/wizard/wizard_missingvisits.py
@@ -24,22 +24,16 @@ class WizardMissingVisits(osv.TransientModel):
         self.env.cr.execute(
             "select bank_code,branch_code,street,city,force_code,rf_id from res_partner where rf_id is not null and bank_code="+"'"+self.bank_code+"'"+" and rf_id not in (select card_no from mutual_guard_tracking where archive_signal is null and visit_time is not null)")
         missing_branches = self.env.cr.dictfetchall()
-        self.env.cr.execute(
-            "select visit_time,visit_time_two from mutual_guard_tracking where visit_time is null and visit_time_two is null and bank_code="+"'"+self.bank_code+"'")
-        missing_visits_created = self.env.cr.dictfetchall()
 
         if(len(missing_branches)>0):
-            if(len(missing_visits_created) == 0):
-                for branch in missing_branches:
-                    self.env['mutual.guard.tracking'].create({
-                        'bank_code': branch['bank_code'],
-                        'branch_code': branch['branch_code'],
-                        'address': branch['street'],
-                        'city': branch['city'],
-                        'force_code': branch['force_code'],
-                        'card_no': branch['rf_id']
-                    })
-            else:
-                raise osv.except_osv('Ghor Ye Dekho......', 'Already missing visits create ho chukey hain....')
+            for branch in missing_branches:
+                self.env['mutual.guard.tracking'].create({
+                    'bank_code': branch['bank_code'],
+                    'branch_code': branch['branch_code'],
+                    'address': branch['street'],
+                    'city': branch['city'],
+                    'force_code': branch['force_code'],
+                    'card_no': branch['rf_id']
+                })
         else:
          raise osv.except_osv('Alert.......', 'All branches have been first visited')
