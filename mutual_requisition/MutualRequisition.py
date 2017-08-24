ec18e17eb943735c466994e36a5129bd70e7f387
parkash067 <pk_bscs@yahoo.com>
2017-08-24 12:47:36 +0500

    add workflow of state in RS

--- mutual_requisition/MutualRequisition.py
+++ mutual_requisition/MutualRequisition.py
@@ -7,23 +7,34 @@ from datetime import date,time
 class mutual_requisition(osv.osv):
     _name = "mutual.requisition"
     _columns = {
+        'state': fields.selection([('pending', 'Pending'), ('confirmed', 'Confirmed')], 'State', store=True,
+                                  default='draft', track_visibility='onchange'),
         'title': fields.char('Title',store=True),
-        'date':fields.date('Date',store=True),
+        'date': fields.date('Date',store=True),
         # 'receipt_no': fields.char('Reciept No',store=True),
         'products': fields.one2many('products.req','product_tb','Products',store=True),
-        'req_type':fields.selection([('New Installation','New Installation'),('Faulty','Faulty'),('Additional','Additional'),('none',' ')],'Requsition Type')
+        'req_type': fields.selection([('New Installation','New Installation'),('Faulty','Faulty'),('Additional','Additional'),('none',' ')],'Requsition Type')
     }
     _defaults={
         'req_type':'none',
     }
+
+    @api.multi
+    def validate(self):
+        return self.write({'state': 'confirmed'})
+
+    @api.multi
+    def cancel(self):
+        return self.write({'state': 'pending'})
+
 
 class products(osv.osv):
     _name = 'products.req'
     _columns = {
         'product_tb': fields.many2one('product.template','Products',store=True,required=True),
         'product_name': fields.many2one('product.template', 'Name', store=True,required=True),
-        'quantity':fields.float('Quantity',store=True,required=True),
+        'quantity': fields.float('Quantity',store=True,required=True),
         'type': fields.selection([('For Technician', 'For Technician'), ('For Customer', 'For Customer')], 'Type', store=True),
-        'customer':fields.many2one('res.partner','Customer',store=True),
-        'ref_to':fields.char('Reference',store=True)
+        'customer': fields.many2one('res.partner','Customer',store=True),
+        'ref_to': fields.char('Reference',store=True)
     }
