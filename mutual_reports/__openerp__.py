cc7fe793827cc068118134f9970db59bf45f59ba
mahsannas109 <ahsnasser_23@hotmail.com>
2017-08-30 16:08:04 +0500

    courier sheet report added in mutual_reporting

--- mutual_reports/__openerp__.py
+++ mutual_reports/__openerp__.py
@@ -21,6 +21,7 @@
     ],
     'data': [
             'mutual_reports_reports.xml',
+            'views/custom_courier_sheet_report.xml',
             'views/custom_report_mutual_req_pdf.xml',
             'views/custom_layouts.xml',
             'views/report_acknowledgment_receipt.xml',
