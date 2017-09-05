1c88d1a6b9b9740981dd4204ffc78f10d09eb493
mahsannas109 <ahsnasser_23@hotmail.com>
2017-09-05 14:39:46 +0500

    doesnot allow count to go negative

--- mutual_accounts_inventory/account_invoice.py
+++ mutual_accounts_inventory/account_invoice.py
@@ -10,6 +10,7 @@ from dateutil.relativedelta import *
 from openerp.tools import amount_to_text_en
 
 
+
 class mutual_account_invoice(osv.osv):
     _inherit = 'account.invoice'
 
@@ -31,14 +32,36 @@ class mutual_account_invoice(osv.osv):
             'inv_id': self.id,
         })
 
+    def checkNeg(self,line):
+        sale_rec=0
+        open_rec=0
+        self.env.cr.execute("select sum(sale_count) sales from inventory_logs where item_code =" + "'" + str(line.product_id.id) + "'")
+        product_stock = self.env.cr.dictfetchall()
+        if any(d['sales'] == None for d in product_stock):
+            sale_rec=0
+        else:
+            for item in product_stock:
+                sale_rec=item["sales"]
+        self.env.cr.execute("select opening_count from inventory_opening where item_code=" + "'" + str(line.product_id.id) + "'" )
+        opening_stock=self.env.cr.dictfetchall()
+        for item in opening_stock:
+            open_rec=item["opening_count"]
+        remain_count=open_rec-(sale_rec+line.quantity)
+        if(remain_count>=0):
+            return 0
+        else:
+            return 1
+
     @api.multi
     def invoice_validate(self):
         for line in self.invoice_line:
             if(line.product_id.type != 'service'):
-                if(self.partner_id.customer):
+                if(self.partner_id.customer and self.checkNeg(line)==0):
+                    self.createLogs(line,self.partner_id.customer,self.partner_id.supplier)
+                elif(self.checkNeg(line)==0):
                     self.createLogs(line,self.partner_id.customer,self.partner_id.supplier)
                 else:
-                    self.createLogs(line,self.partner_id.customer,self.partner_id.supplier)
+                    raise osv.except_osv('Error....', 'product count might go into exceeding to negative')
         return self.write({'state': 'open'})
 
     @api.depends('state')
