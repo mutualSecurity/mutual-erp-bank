248a2214c8b9e500fbbd30f4da70d9ddd91d2582
parkash067 <pk_bscs@yahoo.com>
2017-10-18 15:26:24 +0500

    Show only new variant products on invoice level + incoming date on quant should be current date

--- mutual_inventory/mutualproducts.py
+++ mutual_inventory/mutualproducts.py
@@ -1,10 +1,49 @@
 #The file name of this file must match the filename name which we import in __init__.py file
+from datetime import date, datetime
+from dateutil import relativedelta
 import openerp.addons.decimal_precision as dp
 from openerp.osv import fields, osv
 from openerp.tools.float_utils import float_round, float_compare
 from openerp import api
+from openerp import SUPERUSER_ID, api
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
 
+class mutual_stock_quants(osv.osv):
 
+    _inherit = "stock.quant"
+
+    def quants_reserve(self, cr, uid, quants, move, link=False, context=None):
+
+        '''This function reserves quants for the given move (and optionally given link). If the total of quantity reserved is enough, the move's state
+        is also set to 'assigned'
+
+        :param quants: list of tuple(quant browse record or None, qty to reserve). If None is given as first tuple element, the item will be ignored. Negative quants should not be received as argument
+        :param move: browse record
+        :param link: browse record (stock.move.operation.link)
+        '''
+        toreserve = []
+        reserved_availability = move.reserved_availability
+        #split quants if needed
+        for quant, qty in quants:
+            if qty <= 0.0 or (quant and quant.qty <= 0.0):
+                raise osv.except_osv(_('Error!'), _('You can not reserve a negative quantity or a negative quant.'))
+            if not quant:
+                continue
+            self._quant_split(cr, uid, quant, qty, context=context)
+            toreserve.append(quant.id)
+            reserved_availability += quant.qty
+        #reserve quants
+        if toreserve:
+            self.write(cr, SUPERUSER_ID, toreserve, {'reservation_id': move.id, 'in_date': datetime.now().strftime(DEFAULT_SERVER_DATETIME_FORMAT)}, context=context)
+            #if move has a picking_id, write on that picking that pack_operation might have changed and need to be recomputed
+            if move.picking_id:
+                self.pool.get('stock.picking').write(cr, uid, [move.picking_id.id], {'recompute_pack_op': True}, context=context)
+        #check if move'state needs to be set as 'assigned'
+        rounding = move.product_id.uom_id.rounding
+        if float_compare(reserved_availability, move.product_qty, precision_rounding=rounding) == 0 and move.state in ('confirmed', 'waiting')  :
+            self.pool.get('stock.move').write(cr, uid, [move.id], {'state': 'assigned'}, context=context)
+        elif float_compare(reserved_availability, 0, precision_rounding=rounding) > 0 and not move.partially_available:
+            self.pool.get('stock.move').write(cr, uid, [move.id], {'partially_available': True}, context=context)
 
 
 class mutual_products(osv.osv):
