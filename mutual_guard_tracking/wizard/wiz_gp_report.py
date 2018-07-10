b74731571496724d06a83d8aa35bd8090fab0b45
parkash067 <pk_bscs@yahoo.com>
2018-07-10 21:07:22 +0500

    add bank_code and restrictions

--- mutual_guard_tracking/wizard/wiz_gp_report.py
+++ mutual_guard_tracking/wizard/wiz_gp_report.py
@@ -9,7 +9,10 @@ class WizardReportPdf(osv.TransientModel):
 
     _columns = {
         'date_from': fields.date('From'),
-        'date_to': fields.date('To')
+        'date_to': fields.date('To'),
+        'bank_code': fields.selection([('ABL','ABL'), ('MBL','MBL'), ('DIB','DIB'),
+                                       ('UBL', 'UBL'), ('HBL','HBL'), ('TMBL','TMBL'),
+                                       ('JSBL','JSBL'), ('SAMBA','SAMBA'), ('NBP','NBP')], 'Bank Code')
     }
 
     _defaults = {
@@ -18,11 +21,17 @@ class WizardReportPdf(osv.TransientModel):
     }
     
     def generate_report(self):
-        self.env.cr.execute("""select * from mutual_guard_tracking where archive_signal=False""")
-        return self.env.cr.dictfetchall()
-    
+        self.env.cr.execute("""select * from mutual_guard_tracking where archive_signal=False and bank_code='%s'"""%(self.bank_code))
+        res = self.env.cr.dictfetchall()
+        if len(res)>0:
+            return self.env.cr.dictfetchall()
+        else:
+            raise osv.except_osv(('Error'), ('Active signals are not found of this bank'))
+
     @api.multi
     def print_report(self):
+        if self.date_to > fields.date.today() or self.date_from > fields.date.today():
+            raise osv.except_osv(('Error'), ('Date must be less than today'))
         return {
             'type': 'ir.actions.report.xml',
             'name': 'mutual_guard_tracking.wiz_patrolling_report_pdf',
