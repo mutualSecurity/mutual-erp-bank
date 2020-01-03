0b98bad9a3a291553d9be3ab9d7e8c6b48b9bf48
parkash067 <pk_bscs@yahoo.com>
2020-01-03 19:00:18 +0500

    add module

---
+++ oc_mutual_customization/__openerp__.py
@@ -0,0 +1,22 @@
+
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Odoo Concepts. (<www.odooconcepts.com>)
+# Copyright(c): 2019-Present Odoo Concepts.
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+{
+    'name': 'Mutual Customization',
+    'author': 'Odoo Concepts',
+    'summary': 'Add source document and invoice id on invoice tree view',
+    'depends': ['base', 'account'],
+    'data': ['views/account_invoice_view.xml'],
+    'category': '',
+    'website': "https//:www.odooconcepts.com",
+    'installable': True,
+    'auto_install': False,
+}
